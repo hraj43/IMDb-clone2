daf7137031e876c91799716a32c847c5ecbcfeaf
harshraj438524 <21bec050@iiitdmj.ac.in>
2023-12-15 09:40:47 +0530

    About page added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function about() {
+  return (
+    <div className="max-w-5xl m-auto p-8 space-y-2">
+      <h1 className="text-2xl font-bold text-amber-600">About</h1>
+      <p className="text-stone-200">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem rem
+        blanditiis tempore exercitationem. Dolor iste tempora ea similique totam
+        suscipit maiores rem, labore modi tempore. Obcaecati asperiores
+        voluptates placeat nesciunt iste aut fugiat tenetur?  
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, tempore
+        repellat quas ea saepe iure incidunt cum vero quisquam eos, quam
+        recusandae omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente saepe
+        quidem dolores corporis ut nemo voluptatum modi laboriosam expedita
+        eaque, esse suscipit labore! Molestias facilis laudantium aspernatur
+        blanditiis! Possimus delectus dolore nisi deleniti voluptatum expedita
+        quam et explicabo, quos praesentium culpa nihil voluptates inventore
+        rerum laborum, quia omnis error dignissimos!
+      </p>
+    </div>
+  );
+}
+
+export default about;
