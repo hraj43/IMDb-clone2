e8757206d50f89d25e3a165e9bb69509dc710bce
harshraj438524 <21bec050@iiitdmj.ac.in>
2023-12-16 12:49:45 +0530

    loading effect added

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='flex justify-center mt-20'>
+        <img src="loading1.svg" alt="loadind.." />
+    </div>
+  )
+}
+
+export default loading
