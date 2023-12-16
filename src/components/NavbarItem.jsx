87e4ef1ebc6f4c132410842b32436f7cfe002700
harshraj438524 <21bec050@iiitdmj.ac.in>
2023-12-16 09:51:25 +0530

    Navbar added

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,20 +1,27 @@
-"use client"
-import React from 'react'
-import Link from 'next/link'
-import { useSearchParams } from 'next/navigation'
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
-function NavbarItem({title,param}) {
-    const searchParams=useSearchParams();
-    console.log(searchParams);
-    const genre=searchParams.get("genre");
-    console.log(genre);
+function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+
+  const genre = searchParams.get("genre");
+
   return (
-    <div className='text-lg hover:text-amber-600'>
-        <Link className={`font-semibold ${genre&&genre===param && "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"}`} href= {`\?genre=${param}`}>
+    <div className="text-lg hover:text-amber-600">
+      <Link
+        className={`font-semibold ${
+          genre &&
+          genre === param &&
+          "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+        }`}
+        href={`\?genre=${param}`}
+      >
         {title}
-        </Link>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default NavbarItem
+export default NavbarItem;
